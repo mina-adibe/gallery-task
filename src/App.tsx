2a83648358220e3f6c3446d3a807cbda82e7c47a
Mina Bessada <mina.adibe@gmail.com>
2024-03-20 22:07:39 -0400

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import { Badge } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function App() {
+  return (
+    <>
+      <p>Hello world </p>
+    </>
+  );
+}
+
+export default App;
