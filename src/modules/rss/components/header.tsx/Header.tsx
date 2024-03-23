bc30b90dacf1f9c6266a2ae8e54db7d977fcfe7b
Mina Bessada <mina.adibe@gmail.com>
2024-03-23 13:46:30 -0400

    feat: implement header component

---
+++ src/modules/rss/components/header.tsx/Header.tsx
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1> CNN.com - RSS Channel - App International Edition</h1>
+    </header>
+  );
+};
+
+export default Header;
