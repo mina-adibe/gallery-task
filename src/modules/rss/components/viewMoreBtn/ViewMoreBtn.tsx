4176914af6dc516dece242966197bd148df253da
Mina Bessada <mina.adibe@gmail.com>
2024-03-23 13:53:52 -0400

    feat: implement view more btn

---
+++ src/modules/rss/components/viewMoreBtn/ViewMoreBtn.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const ViewMoreBtn = ({ onClick, disabled }) => {
+  return (
+    <div className="button-container">
+      <button className="btn" onClick={onClick} disabled={disabled}>
+        {disabled ? "Loading..." : "View More"}
+      </button>
+    </div>
+  );
+};
+
+export default ViewMoreBtn;
