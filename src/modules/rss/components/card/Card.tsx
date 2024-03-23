e0465d2d4b2755cea3acd942ddf29d151072f18c
Mina Bessada <mina.adibe@gmail.com>
2024-03-23 13:45:28 -0400

    feat: implement card component

---
+++ src/modules/rss/components/card/Card.tsx
@@ -0,0 +1,18 @@
+import "./styles.scss";
+const Card = ({ title, date, imgSrc }: any) => {
+  return (
+    <>
+      <div className="card">
+        <div className="card__item">
+          <img src={imgSrc} alt="image" className="card__img" />
+        </div>
+        <div className="card__content">
+          <h3 className="card__title">{title}</h3>
+          <p className="card__date">14/12/1988</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
