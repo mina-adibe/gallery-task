42b97e3f81fac51368dffeb3a6499f95bd6fcef6
Mina Bessada <mina.adibe@gmail.com>
2024-03-23 19:46:01 -0400

    feat: implement url state sync

--- src/modules/rss/hooks/UseFetchPhotos.tsx
+++ src/modules/rss/hooks/UseFetchPhotos.tsx
@@ -1,20 +1,29 @@
-import axios from "axios";
-import React from "react";
-import { baseUrl } from "../../../constants/constants";
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { api } from '../../../api/api';
 
 const UseFetchPhotos = () => {
+  const [searchParams] = useSearchParams();
   const [posts, setPosts] = React.useState([]);
-  const [page, setPage] = React.useState(1);
-  const [limit, setLimit] = React.useState(7);
   const [error, setError] = React.useState(null);
-  const [hasMoreData, setHasMoreData] = React.useState(true);
-  const [isFetching, setIsFetching] = React.useState(false);
+  const [hasMoreData, setHasMoreData] = React.useState<boolean>(true);
+  const [isFetching, setIsFetching] = React.useState<boolean>(false);
 
-  baseUrl;
-  React.useEffect(() => {
-    setIsFetching(true);
-    axios
-      .get(`${baseUrl}/?_page=${page}&_limit=${limit}`)
+  //check if the limit is set in the url query params and set the default limit
+  const limitString = searchParams.get('_limit');
+  const defaultLimit = 7;
+  const parsedLimit = limitString ? parseInt(limitString) : defaultLimit;
+  //check if the page is set in the url query params and set the default page
+  const pageString = searchParams.get('_page');
+  const defaultPage = 1;
+  const parsedPage = pageString ? parseInt(pageString) : defaultPage;
+
+  const [page, setPage] = React.useState<number>(parsedPage);
+  const [limit, setLimit] = React.useState<number>(parsedLimit);
+
+  const fetchPhotos = (offset: any) => {
+    api
+      .get(`photos/${offset}`)
       .then((response) => {
         setPosts((prevPosts) => [...prevPosts, ...response.data]);
 
@@ -30,9 +39,13 @@ const UseFetchPhotos = () => {
       .finally(() => {
         setIsFetching(false);
       });
-  }, [page]);
+  };
 
-  // limit, setLimit : you can use it to change the limit of the data in the future
+  React.useEffect(() => {
+    const offsetPath = `?_page=${page}&_limit=${limit}`;
+    fetchPhotos(offsetPath);
+  }, [page, limit]);
+
   return { posts, setPosts, page, setPage, limit, setLimit, error, hasMoreData, isFetching };
 };
 
--- src/modules/rss/views/List.tsx
+++ src/modules/rss/views/List.tsx
@@ -1,33 +1,35 @@
-import Card from "../components/card/Card";
-import "./styles.scss";
-import UseFetchPhotos from "../hooks/UseFetchPhotos";
-import Header from "../components/header.tsx/Header";
-import ViewMoreBtn from "../components/viewMoreBtn/ViewMoreBtn";
+import Card from '../components/card/Card';
+import './styles.scss';
+import UseFetchPhotos from '../hooks/UseFetchPhotos';
+import Header from '../components/header.tsx/Header';
+import ViewMoreBtn from '../components/viewMoreBtn/ViewMoreBtn';
+import { useSearchParams } from 'react-router-dom';
 
 const List = () => {
-  const { posts, setPage, error, hasMoreData, isFetching } = UseFetchPhotos();
+  const { posts, page, setPage, limit, error, hasMoreData, isFetching } = UseFetchPhotos();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const blogCards = posts.map((blog) => {
     return (
-      <div className="item">
-        <Card key={blog.id} title={blog.title} date={blog.date} imgSrc={blog.url} />
+      <div className="item" key={blog.id}>
+        <Card title={blog.title} date={blog.date} imgSrc={blog.url} />
       </div>
     );
   });
 
   const loadMoreBlogs = () => {
     setPage((prevPage) => prevPage + 1);
+    searchParams.set('_page', page.toString());
+    searchParams.set('_limit', limit.toString());
+
+    setSearchParams(searchParams);
   };
 
   return (
     <section className="main__container">
       <Header />
       <div className="list__container">{blogCards}</div>
-      {hasMoreData ? (
-        <ViewMoreBtn onClick={loadMoreBlogs} disabled={isFetching} />
-      ) : (
-        <div>No more items to load.</div>
-      )}
+      {hasMoreData ? <ViewMoreBtn onClick={loadMoreBlogs} disabled={isFetching} /> : <div>No more items to load.</div>}
       {error && <div className="error">{error}</div>}
     </section>
   );
